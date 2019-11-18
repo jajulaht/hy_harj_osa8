401943a3887f30fc7c0c9efc2e82f88b090a25aa
Janne Lahtinen <54285341+jajulaht@users.noreply.github.com>
2019-11-18 18:22:19 +0200

    8.4 done

--- index.js
+++ index.js
@@ -103,9 +103,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
-    filterBooks(name: String!): [Book!]
-    filterAndCountBooks(name: String!): Int!
+    allBooks(author: String): [Book!]
     allAuthors: [Author!]!
   }
 `
@@ -114,7 +112,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter(b => b.author === args.author)
+    },
+
     allAuthors: () => authors,
   },
   Author: {

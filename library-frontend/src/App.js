d7d04ebf4207b789891915630dbcbf6b60703d35
Janne Lahtinen <54285341+jajulaht@users.noreply.github.com>
2019-11-25 22:23:37 +0200

    8.8 done

---
+++ library-frontend/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const ALL_AUTHORS = gql`
+  {
+    allAuthors  {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const authors = useQuery(ALL_AUTHORS)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        result={authors}
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+//import { useApolloClient } from '@apollo/react-hooks'
+
+const Authors = ({ result, show }) => {
+  if (!show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {result.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
